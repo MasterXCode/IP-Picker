c66308b76ec617373e39887209894563325a2fa2
MASTERX <50529518+seifsherifssss@users.noreply.github.com>
2019-07-11 17:51:43 +0200

    Add files via upload

---
+++ IP.py
@@ -0,0 +1,13 @@
+import socket 
+import sys
+import os
+os.system (" clear && cowthink -f dragon 'Welcome To IP-Picker ' ")
+import os 
+os.system ("echo''")
+print ( 'Coded By MASTERX ')
+print ( 'Enter Your Target DNS (Without http Or https)')
+import os 
+os.system ("echo''")
+host=input("====>>  ")
+IP=socket.gethostbyname(host)
+print("Hostname>> ",host, '\n' "Target IP>> ",IP )
---
+++ setup.sh
@@ -0,0 +1,1 @@
+touch /$PREFIX/bin/ip && chmod 777 /$PREFIX/bin/ip && echo 'python /$HOME/IP-Picker/IP.py' > /$PREFIX/bin/ip
